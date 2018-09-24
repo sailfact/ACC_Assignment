a7df377c1b82d630c05522d1860e3fd1ae498039
sailfact <sailfact@outlook.com>
2018-09-24 13:51:42 +0800

    started pre-threaded server

--- src/acc.h
+++ src/acc.h
@@ -41,4 +41,8 @@ typedef struct {
     long        thread_count;   /* # connections handled */
 } Thread;
 
-Thread *tptr; 
+Thread *tptr;
+#define MAXNCLI 32 
+int clifd[MAXNCLI], iget, iput;
+pthread_mutex_t clifd_mutex;
+pthread_cond_t clifd_cond;
--- src/mess.c
+++ src/mess.c
@@ -1,51 +1,76 @@
 #include	"acc.h"
 
-static void	*doit(void *);		/* each thread executes this function */
+static int nthreads;
+pthread_mutex_t clifd_mutex = PTHREAD_MUTEX_INITIALIZER;
+Pthread_cond_t clifd_cond =PTHREAD_COND_INITIALIZER
 
 static struct client_list *head = NULL;
 static struct client_list *current = NULL;
 
-int main(int argc, char **argv)
-{
-	int		listenfd, *iptr;
-	pthread_t	tid;
-	socklen_t	addrlen, len;
-	struct sockaddr	*cliaddr;
+int main(int argc, char **argv) {
+	int 	i, listenfd, connfd;
+	void	sig_int(int), thread_make(int);
+	socklen_t	addrlen, clilen;
+	struct sockaddr* cliaddr;
 
-	head = (struct client_list*)malloc(sizeof(struct client_list));
-
-	if (argc == 2)
+	if (argc == 3)
 		listenfd = Tcp_listen(NULL, argv[1], &addrlen);
-	else if (argc == 3)
-		listenfd = Tcp_listen(argv[1], argv[2], &addrlen);
+	else if (argc == 4)
+		listenfd = Tcp_lsiten(argv[1], argv[2], &addrlen);
 	else
-		err_quit("usage: tcpserv01 [ <host> ] <service or port>");
+		err_quit("usage: mess [ <host> ] <port#> <#threads>");
+	
+	cliaddr = Malloc(addrlen);
 
-	cliaddr = (struct sockaddr *) Malloc(addrlen);
+	nthreads = atoi(argv[argc - 1]);
+	tptr = Calloc(nthreads, sizeof(Thread));
+	iget = iput = 0;
 
-	for ( ; ; ) {
-		len = addrlen;
-		iptr = (int *) Malloc(sizeof(int));
-		*iptr = Accept(listenfd, cliaddr, &len);
+	/* create all the threads */
+	
+	for(i = 0; i < nthreads; i++) {
+		thread_make(i);		/* only main thread returns */
+	}
+	
+	Signal(SIGINT, sig_int);
 
-		Pthread_create(&tid, NULL, &doit, iptr);
+	while (1) {
+		clilen = addrlen;
+		connfd = Accept(listenfd, cliaddr, &clilen);
+
+		Pthread_mutex_lock(&clifd_mutex);
+		clifd[iput] = connfd;
+		if (++iput = MAXNCLI)
+			iput = 0;
+		if (iput == iget)
+			err_quit("iput = iget = %d", iput);
+		Pthread_cond_signal(&clifd_cond);
+		Pthread_mutex_unlock(&clifd_mutex);
 	}
 }
 
-static void * doit(void *arg)
-{
-	int		connfd;
-
-	connfd = *((int *) arg);
-	free(arg);
-
-	Pthread_detach(pthread_self());
-	str_mailserver(connfd);		/* same function as before */
-	Close(connfd);			/* we are done with connected socket */
-	return(NULL);
+void thread_make(int i) {
+	void 	*thread_main(void *);
+	Pthread_create(&tptr[i].thread_tid, NULL, &thread_main, (void *) i);
+	return;		/* main thread returns */
 }
 
+void * thread_main(void *arg) {
+	int connfd;
+	void web_child(int);
 
-void str_mail(int sockfd) {
+	printf("thread %d starting\n", (int)arg);
+	while (1) {
+		Pthread_mutex_lock(&clifd_mutex);
+		while (iget == iput)
+			Pthread_cond_wait(&clifd_cond, &clifd_mutex);
+		connfd = clifd[iget]; 	/* connected socket to service */
+		if (++iget == MAXNCLI)
+			iget = 0;
+			Pthread_mutex_unlock(&clifd_mutex);
+			tptr[(int)arg].thread_count++;
 
+			web_child(connfd);
+			Close(connfd);
+	}
 }
