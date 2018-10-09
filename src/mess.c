82dade21d3f1586c516fb42e900a7f93f146d676
sailfact <sailfact@outlook.com>
2018-10-09 22:30:28 +0800

    todo cmd_quit cmd_make

--- src/mess.c
+++ src/mess.c
@@ -1,11 +1,17 @@
 #include	"acc.h"
 #include 	"clientlist.h"
 
-void mail_srv(int sockfd);
-int check_command_one(char *arg);
-int check_command_two(char *arg1, char *arg2);
-int check_command_three(char *arg1, char *arg2, char *arg3);
-void addClient(int sockfd);
+void mail_srv(int);
+int check_command_one(int, char *);
+int check_command_two(int, char *, char *);
+int check_command_three(int, char *, char *, char *);
+void cmd_make(int sockfd, char *name);
+void cmd_read(int sockfd, char *name, int id);
+void cmd_delete(int sockfd, char *name, int id);
+void cmd_get_client_list(int sockfd);
+void cmd_get_mailbox(int sockfd, char *name);
+void cmd_quit(int sockfd);
+
 
 static int nthreads;
 pthread_mutex_t clifd_mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -72,7 +78,6 @@ void * thread_main(void *arg)
 			iget = 0;
 			Pthread_mutex_unlock(&clifd_mutex);
 			tptr[(int)arg].thread_count++;
-			addClient(connfd);
 			mail_srv(connfd);
 			Close(connfd);
 	}
@@ -89,17 +94,18 @@ void mail_srv(int sockfd)
 		if ( (n = Readline(sockfd, line, MAXLINE)) == 0)
 			return;		/* connection closed by other end */
         scans = sscanf(line, "%s %s %s", arg1, arg2, arg2);
+		printf("commands %d\n\targ1 = %s\n\targ2 = %s\n\targ3 = %s\n",scans, arg1, arg2, arg3);
         switch (scans) 
         {
-            case 3:
-                check_command_three(arg1, arg2, arg3);
-                break;
-            case 2:
-                check_command_two(arg1, arg2);
-                break;
             case 1:
-                check_command_one(arg1);
+                check_command_one(sockfd, arg1);
                 break;
+			case 2:
+                check_command_two(sockfd, arg1, arg2);
+                break;
+			case 3:
+                check_command_three(sockfd, arg1, arg2, arg3);
+                break;            
             default:
 			    snprintf(line, sizeof(line), "input error\n");
         }
@@ -108,7 +114,7 @@ void mail_srv(int sockfd)
 		Writen(sockfd, line, n);
 	}
 }
-int check_command_one(char *arg) 
+int check_command_one(int sockfd, char *arg) 
 {
 	int len = strlen(arg);
     char str[sizeof(arg)+1];
@@ -116,14 +122,20 @@ int check_command_one(char *arg)
             str[i] = tolower(arg[i]);
 
     if (strcmp(str, "quit") == 0)
-        printf("make\n");
+    {
+		printf("quit\n");
+		cmd_quit(sockfd);
+	}
     else if (strcmp(str, "get_client_list") == 0)
-        printf("get_client_list\n");
+    {
+		printf("get_client_list\n");
+		cmd_get_client_list(sockfd);
+	}
 
 	return 0;
 }
 
-int check_command_two(char *arg1, char *arg2) 
+int check_command_two(int sockfd, char *arg1, char *arg2) 
 {
 	int len = strlen(arg1);
     char str[sizeof(arg1)+1];
@@ -131,14 +143,20 @@ int check_command_two(char *arg1, char *arg2)
             str[i] = tolower(arg1[i]);
 
     if (strcmp(str, "make") == 0)
-        printf("make %s\n", arg2);
+ 	{
+	    printf("make %s\n", arg2);
+		cmd_make(sockfd, arg2);
+	}
     else if (strcmp(str, "get_mailbox") == 0)
+	{
         printf("get_mailbox %s\n", arg2);
+		cmd_get_mailbox(sockfd, arg2);
+	}
 
     return 0;
 }
 
-int check_command_three(char *arg1, char *arg2, char *arg3) 
+int check_command_three(int sockfd, char *arg1, char *arg2, char *arg3) 
 {
 	printf("cmd%s name%s id%s\n", arg1, arg2, arg3);
 	int len = strlen(arg1);
@@ -147,31 +165,48 @@ int check_command_three(char *arg1, char *arg2, char *arg3)
             str[i] = tolower(arg1[i]);
         
     if (strcmp(str, "read") == 0)
-        printf("read %s %s\n", arg2, arg3);
+    {    
+		printf("read %s %s\n", arg2, arg3);
+		cmd_read(sockfd, arg2, atoi(arg3));
+	}
     else if (strcmp(str, "delete") == 0)
-        printf("delete %s %s\n", arg2, arg3);
-
+    {
+	    printf("delete %s %s\n", arg2, arg3);
+		cmd_delete(sockfd, arg2, atoi(arg3));
+	}
     return 0;
 }
 
-void addClient(int sockfd) 
+void cmd_make(int sockfd, char *name)
 {
-	struct client newClient;
-	char buff[MAXLINE];
-	const char *ptr;
-	time_t rawtime;
-	struct tm *timeinfo;
-	socklen_t len;
-	struct sockaddr_in cliaddr;
-	Getpeername(sockfd, (SA *) &cliaddr, &len);
-	printf("%s\n", Inet_ntop(AF_INET, &cliaddr.sin_addr, buff, sizeof(buff)));
-	strcpy(newClient.client_name, "client");
-	time(&rawtime);
-	timeinfo = localtime(&rawtime);
-	strcpy(newClient.time_joined, asctime(timeinfo));
-	strcpy(newClient.ip_address, ptr);
-	newClient.ip_port = ntohs(cliaddr.sin_port);
-	newClient.email_counter = 0;
-	insertFirst(newClient);
-	printList();
+
+}
+
+void cmd_read(int sockfd, char *name, int id)
+{
+
+}
+
+void cmd_delete(int sockfd, char *name, int id)
+{
+
+}
+
+void cmd_get_client_list(int sockfd)
+{
+	
+}
+
+void cmd_get_mailbox(int sockfd, char *name)
+{
+
+}
+
+void cmd_quit(int sockfd)
+{
+	printf("quittig...\n");
+	char line[MAXLINE];
+	snprintf(line, sizeof(line), "input error\n");
+	Writen(sockfd, line, sizeof(line));
+	Close(sockfd);
 }
